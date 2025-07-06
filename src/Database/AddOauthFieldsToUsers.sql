d65a58586f34fd5c9eb328eb3141840bc820e094
Frédéric Blanc <frederic.blanc@itanea.fr>
2025-07-07 00:48:04 +0200

    feat: change unique index to users.email. waiting the migration of auth/user implement it directly.

--- src/Database/AddOauthFieldsToUsers.sql
+++ src/Database/AddOauthFieldsToUsers.sql
@@ -6,5 +6,6 @@ ALTER TABLE `users` ADD `oauth_user_email` VARCHAR(255);
 ALTER TABLE `users` ADD `oauth_user_data` TEXT;
 ALTER TABLE `users` ADD `oauth_access_token` TEXT;
 ALTER TABLE `users` ADD `oauth_refresh_token` TEXT;
-CREATE UNIQUE INDEX unique_oauth_provider_user_id ON `users` (`oauth_provider`, `oauth_user_id`);
+CREATE UNIQUE INDEX unique_email ON `users` (`email`);
+
 COMMIT;
